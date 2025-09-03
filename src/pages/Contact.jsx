b531793d76771d70de8dc26b7ca630638c168c86
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-09-03 11:03:49 +0200

    hey

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+export function Contact(){
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>Welcome to the Contact Page!</p>
+        </div>
+    );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,8 @@
+export function Projects(){
+    return (
+        <div>
+            <h1>Projects Page</h1>
+            <p>Welcome to the Projects Page!</p>
+        </div>
+    );
+}
