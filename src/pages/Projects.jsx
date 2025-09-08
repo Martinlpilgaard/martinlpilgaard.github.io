d0bcfc44647ac2dda1819f200cf36b7d4e8baddd
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-09-08 09:50:31 +0200

    hey

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,29 @@
-export default function Projects(){
-    return (
-        <div>
-            <h1>Projects Page</h1>
-            <p>Welcome to the Projects Page!</p>
+
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router";
+
+export default function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("/data/projects.json");
+      const data = await response.json();
+      setClients(data);
+    }
+    getData();
+  }, []);
+  return (
+    <section id="projects">
+      <div className="headline-container">
+        <h2>Projects</h2>
+      </div>
+      {projects.map((project) => (
+        <div key={project.id}>
+          <NavLink to={`/projects/${project.id}`}>Læs mere</NavLink>
         </div>
-    );
+      ))}
+    </section>
+  );
+  console.log(projects);
 }
