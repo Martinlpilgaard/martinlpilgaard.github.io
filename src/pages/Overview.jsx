89bc1410d4f2765c02cbf5b44ef6dbe3bdd44f07
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-09-18 17:34:19 +0200

    test

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,12 +1,10 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import OverviewProjects from "../sections/OverviewProjects";
 
 export default function Overview() {
   return (
     <>
       <Header />
-      <OverviewProjects />
       <Footer />
     </>
   );
