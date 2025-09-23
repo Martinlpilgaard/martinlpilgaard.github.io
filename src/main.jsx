ef4347438aadfbc03a772ab905e1fcef1754f214
Martinlpilgaard <eaa24mlpk@students.eaaa.dk>
2025-09-23 10:07:22 +0200

    mobile overriders

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import ScrollToTop from "./components/ScrollToTop.jsx";
+import "./styles/mobile-overrides.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
